337b50fa06344d9c99204aab6c6e78b2ca2f59c9
Github1234gfh <83724836+Github1234gfh@users.noreply.github.com>
2022-12-07 01:57:00 +0300

    update

--- ws/wjf/views.py
+++ ws/wjf/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
 	if request.method == 'POST':
-		name = request.POST.get('name')
-		call = request.POST.get('call')
-		return HttpResponse(f'{name} , {call}')
+		return HttpResponseRedirect('appointment/')
 	return render(request, 'index.html')
+
+def appointment(request):
+	name = request.POST.get('name')
+	email = request.POST.get('email')
+	call = request.POST.get('call')
+	sel = request.POST.get('sel')
+	text = request.POST.get('text')
+
+	return HttpResponse(f''' 
+		<p> name - {name}</p>
+		<p> email - {email}</p>
+		<p> call - {call}</p>
+		<p> sel - {sel}</p>
+		<p> text - {text}</p>
+	''')
--- ws/ws/urls.py
+++ ws/ws/urls.py
@@ -6,4 +6,5 @@ from wjf import views
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', views.index),
+	path('appointment/', views.appointment)
 ]
