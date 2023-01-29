92102c6fc70f41f95347bd1273023ede95d234fc
Github1234gfh <83724836+Github1234gfh@users.noreply.github.com>
2023-01-30 00:45:45 +0300

    del backup

--- app/backup/src/Components/page/GetDate.jsx
+++
@@ -1,7 +0,0 @@
-export const getDate = (date) => {
-	const PerDate = date.split('.')
-	PerDate.push(PerDate[0])
-	PerDate[0] = PerDate[2]
-	delete PerDate[2]
-	return new Date(PerDate.map(elem => {return Number(elem)})) < new Date() - 86400000
-}
